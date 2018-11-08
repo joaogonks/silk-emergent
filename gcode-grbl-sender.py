3975990581f495c8fdf32ee496e5dccfc97cb014
João Costa <joaopedrocosta@me.com>
2018-11-08 13:37:44 -0500

    it is working now

--- gcode-grbl-sender.py
+++ gcode-grbl-sender.py
@@ -7,19 +7,21 @@ import sys
 usb_port = sys.argv[1]
 # Open grbl serial port
 s = serial.Serial(str(usb_port),115200)
- 
-s.write("\r\n\r\n") # Wake up grbl
+wakeup_message = "\r\n\r\n"
+s.write(wakeup_message.encode()) # Wake up grbl
 time.sleep(2)   # Wait for grbl to initialize
 s.flushInput()  # Flush startup text in serial input
  
 # Stream g-code to grbl
 try:
     while True:
-        gcode_to_send = raw_input("Please type your gcode.")
-        l = line.strip() # Strip all EOL characters for streaming
-        print('Sending: ' + l)
-        s.write(l + '\n') # Send g-code block to grbl
+        gcode_to_send = input("Please type your gcode.")
+        l = gcode_to_send.strip() # Strip all EOL characters for streaming
+        l = l + '\n'
+        l = l.encode()
+        print('Sending: ' + str(l))
+        s.write(l) # Send g-code block to grbl
         grbl_out = s.readline() # Wait for grbl response with carriage return
-        print(' : ' + grbl_out.strip())
+        print(grbl_out.strip())
 except KeyboardInterrupt:
     s.close()
